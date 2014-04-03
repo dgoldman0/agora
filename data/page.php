2137dc757f24f5f8661d8c387d6b2306d7ce6f0f
n02647459 <n02647459@hawkmail.newpaltz.edu>
2014-04-03 08:56:54 -0400

    Fixed page.php

--- data/page.php
+++ data/page.php
@@ -1,19 +1,17 @@
 <?
 require_once 'data.php';
 
-class Page extends 
+class Page extends BaseObject
 {
 	const TYPE_NORMAL = 1;
-	public $title, $perma, $shop_id, $tstamp, $content, $type, $id;
-	function __construct($title, $perma, $shop_id, $tstamp, $content, $type, $id)
+	public $title, $perma, $shop_id, $content, $type;
+	function __construct($title, $perma, $shop_id, $content, $type)
 	{
 		$this->title = $title;
 		$this->perma = $perma;
 		$this->shop_id = $shop_id;
-		$this->tstamp = $tstamp;
 		$this->content = $content;
 		$this->type = $type;
-		$this->id = $id;
 	}
 	function getAbstract()
 	{
@@ -23,9 +21,54 @@ class Page extends
 		else
 			return substr($content, 0, $pos);
 	}
-	function getCategories()
+	public static function get($id)
 	{
-		
+	}
+	public static function getFromRow($row)
+	{
+			$page = new Page($row['title'], $row['perma'], $row['shop_id'], $row['content'], $row['type']);
+			$page.init();
+			return $page;
+	}
+	public function write()
+	{
+		if (!$this->live)
+		{
+			$con = BaseObject::$con;
+			$sql = "INSERT INTO pages (title, perma, shop_id, type, content) VALUES (?,?,?,?,?);";
+			$stmt = $con->prepare($sql);
+			$stmt->bind_param('ssiis', $this->title, $this->perma, $this->shop_id, $this->type, $this->content);
+			$stmt->execute();
+			$stmt->close();
+			return $con->insert_id;
+		}
+	}
+	// I want to replace these text errors with an error hierarchy
+	public static function validate()
+	{
+		$errors = array();
+		if ($this->perma)
+		{
+			if (permaExists($this->perma))
+				$errors[] = "Perma Exists";
+		} else
+		{
+			$error = "Perma Blank";
+		}
+		if ($shop_id)
+		{
+			if ($shop_id >= 0)
+			{
+				// Check if shop exists
+				if (!Shop::exists($shop_id))
+				{
+					$errors[] = "Invalid Shop";
+				}
+			} else {
+				$errors[] = "Invalid Shop";
+			}
+		}
+		return $errors;
 	}
 }
 class PageLink
